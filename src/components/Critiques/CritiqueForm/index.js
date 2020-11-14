8bcdb51c073019daf99c69d4be28ba3ca4c93767
ArnoSlb <soulabaille.arnaud@gmail.com>
2020-11-14 12:53:55 +0100

    critique presse wip

--- src/components/Critiques/CritiqueForm/index.js
+++ src/components/Critiques/CritiqueForm/index.js
@@ -1,11 +1,22 @@
-import react from "react";
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './styles.scss';
 
-const CritiqueForm = () => (
-    <div className="critique-form">Voici le formulaire pour poster une critique</div>
-);
+const CritiqueForm = () => {
 
-export default CritiqueForm
+    const HandleOnChange = () => {
+        console.log('je passe dans Handle On Change')
+    }
+
+    return (
+        <form className="critique-form">
+            <div>
+                <label htmlFor="title">Titre :</label>
+                <textarea type="text" id="title" name="critical_title"> </textarea>
+            </div>
+        </form>
+    );
+}
+
+export default CritiqueForm;
--- src/components/Critiques/index.js
+++ src/components/Critiques/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 import CritiqueDeLaSemaine from 'src/components/Critiques/CritiqueDeLaSemaine';
 import CritiqueFormLink from 'src/components/Critiques/CritiqueFormLink';
+import CritiquePresse from 'src/components/Critiques/CritiquePresse';
 
 import './styles.scss';
 
 const Critiques = () => (
   <div className="critiques">
     <CritiqueDeLaSemaine />
-    <CritiqueFormLink />
+    <div className="critiques__form-et-presse">
+      <CritiqueFormLink />
+      <CritiquePresse />
+    </div>
   </div>
 );
 
